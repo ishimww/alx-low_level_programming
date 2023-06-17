4e42d8997e8346b59d946ee2401041457267ecfc
ishimww <uwimpayeclementine12@gmail.com>
2023-06-17 07:08:58 -0700

    c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,9 @@
  * Return :0 (success)
 
  */
-int main(void);
+int main(void)
 {
-	printf("%s,with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
