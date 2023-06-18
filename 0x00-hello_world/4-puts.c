99c0bb48a94748f38912baca6aad99815518f023
ishimww <uwimpayeclementine12@gmail.com>
2023-06-18 10:54:31 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that prints a line with puts statement
  * return: 0 (success)
  *
  */
@@ -8,4 +8,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}		
+}
