41e22b19e31c995e1e4728c2e1e9ba5ce5886b68
ishimww <uwimpayeclementine12@gmail.com>
2023-06-18 11:25:30 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a line with puts statement
- * return: 0 (success)
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
