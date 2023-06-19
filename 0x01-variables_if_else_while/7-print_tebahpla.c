f04c8c92a3c144c723a2f9e6021934e55d3118c4
ishimww <uwimpayeclementine12@gmail.com>
2023-06-19 09:06:28 -0700

    variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,14 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	int n = 97;
+{
+	int n = 122;
 
-	while (n <= 122 )
+	while (n >= 122)
 	{
 		putchar(n);
 		n--;
 	}
 	putchar('\n');
 	return (0);
-}	
+}
