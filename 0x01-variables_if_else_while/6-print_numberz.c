9af677996f9e38580e0612b13a9fa12229065bad
ishimww <uwimpayeclementine12@gmail.com>
2023-06-19 08:47:44 -0700

    variables

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	int n=0;
+{
+	int n = 0;
 	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
+		putchar(n +48);
 	}
 	putchar('\n');
 	return (0);
-}	
+}
