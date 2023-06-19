6eb0f44dffe33ef170b02a613a9dc42c64a3a5fc
ishimww <uwimpayeclementine12@gmail.com>
2023-06-19 07:51:39 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,21 +5,21 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	int n = 97;
 
 	while (n <= 122)
 	{
-		if (n == 113 || n == 103)
-		{	
+		if (n == 71 || n == 65)
+		{
 			n++;
 		        continue;
-		}	
+		}
 	        putchar(n);
 		n++;
 	}
 	putchar('\n');
-	return (0);	
-}	 
-		     
-	
+	return (0);
+}
+
+
