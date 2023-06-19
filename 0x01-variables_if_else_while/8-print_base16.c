b2370092a9eb66ef53f36c5bde1134cbf49daab4
ishimww <uwimpayeclementine12@gmail.com>
2023-06-19 09:36:21 -0700

    variables

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	int n;
-        int m;	
+int n;
+int m;
 
-	for (n = 48; n <= 57; n++)
-	{	
-		putchar(n);
-	}
-	for (m = 97; m <= 122; m++)
-	{
-		putchar(m);
-	}
-	putchar('\n');
-	return (0);
-}	
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (m = 97; m <= 102; m++)
+{
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
 
