cf2bc3c14ebb1cdba33d04e0a0171974d120011c
ishimww <uwimpayeclementine12@gmail.com>
2023-06-19 11:36:54 -0700

    variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,21 +10,21 @@ int main(void)
 	int j;
 
 	for (i = 0; i <= 8; i++)
-	for (j = i + 1; j <= 9; j++)
-	{
-
+	{	
+	   for (j = i + 1; j <= 9; j++)
+	   {   	   
 		putchar(i + '0');
 		putchar(j + '0');
-		if (j + '0' = 89)
-		{
-			continue;
-		}	
+
+		if (!(i == 8 &&  j == 9))
+		
+			
 
 
 		putchar(',');
 		putchar(' ');
-		}
-	}
+	   }
+        }
 	putchar('\n');
 	return (0);
 }
