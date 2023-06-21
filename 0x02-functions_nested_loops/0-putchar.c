a9433c5d2ccc1160916ee5d771d7552dde92c6b1
ishimww <uwimpayeclementine12@gmail.com>
2023-06-21 13:23:14 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
 	_putchar('H');
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,27 @@
-#include  "main.h"
+#include  "main"
 /**
- * print_alphabets - prints lowercase alphabets
- *
- * return: Always 0 success
- *
- */
-void print_alphabet(void)
+  * print_alphabet_x10 - Make alphabet x10 times
+  *
+  * Return: void
+  */
+void print_alphabet_x10(void)
 {
 	char c;
+	int i = 0;
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (i <= 9)
 	{
-		_putchar(c);
-	}
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+
+		i++;
+	}
 }
+
 
 
 
