6e65c2342aba0abae143ab8c798a0cf4ed06410d
ishimww <uwimpayeclementine12@gmail.com>
2023-06-22 15:06:35 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers - print 0123456789
+*
+*Return: void
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57, a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
+
