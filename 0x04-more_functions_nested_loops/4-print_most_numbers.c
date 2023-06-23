6fe2b44f1c3361dc521deafd4b3e204305b6ba3a
ishimww <uwimpayeclementine12@gmail.com>
2023-06-23 11:27:28 -0700

    loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,8 @@ void print_most_numbers(void)
 
 	for (a = '0'; a <= '9'; a++)
 	{
-		_putchar(a);
-	}
-	for (a == '2' || a == '4')
-	{
-		continue;
+	if (!(a == '2' ||  a== '4'))
+	_putchar(a);
 	}
 	_putchar('\n');
-}
+}	
