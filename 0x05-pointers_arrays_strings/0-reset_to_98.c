aaefeeb7042f7f6656e267aab8d2329b35f783d4
ishimww <uwimpayeclementine12@gmail.com>
2023-06-26 10:38:14 -0700

    aps

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * reset_to_98 - defferencing pointers
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *n;
+
+	n = 45;
+	int *n = &n
+	_putchar(n);
+	if (address of 'n' = *n)
+	{
+		_putchar('%p; &n);
+	}
+	*n = 98
+	_putchar(n);
+	_putchar('\n');
+	return = (0);
+}	
+
+
+	
