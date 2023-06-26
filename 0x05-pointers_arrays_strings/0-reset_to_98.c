fb861f2d6f570404d3986d42e9a85dd6012c3573
ishimww <uwimpayeclementine12@gmail.com>
2023-06-26 13:07:22 -0700

    asp

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,26 +1,14 @@
 #include "main.h"
 /**
- * reset_to_98 - defferencing pointers
- *
- * Return: Always 0.
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value
+ * @n: input
+ * return: n
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int *n;
-
-	n = 45;
-	int *n = &n
-	_putchar(n);
-	if (address of 'n' = *n)
-	{
-		_putchar('%p; &n);
-	}
-	*n = 98
-	_putchar(n);
-	_putchar('\n');
-	return = (0);
-}	
+	*n = 98;
+}
 
 
-	
+
