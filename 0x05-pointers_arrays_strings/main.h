dffb2da86e511a22b9056db056a7d1fd087d0e7a
ishimww <uwimpayeclementine12@gmail.com>
2023-06-28 08:32:32 -0700

    asp

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -11,3 +11,6 @@ char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
