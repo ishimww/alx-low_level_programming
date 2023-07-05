52f69f55194a2dbdf21282557f78e7582969e713
ishimww <uwimpayeclementine12@gmail.com>
2023-07-05 14:06:19 -0700

    asp

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @arr:contains data 
+ *
+ * Return: always zero.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	_memset(s, b, n);
+	return (s);
+}	
+
