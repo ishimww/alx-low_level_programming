af71d1e7e8ffdad80fcc955ae73957d02601e6bb
ishimww <uwimpayeclementine12@gmail.com>
2023-07-05 15:02:02 -0700

    asp

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _memcpy - a function that copies memory area
+ *@i
+ *@n: value of bytes to start when adding
+ *@src: source
+ *@dest: destination
+ * Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
