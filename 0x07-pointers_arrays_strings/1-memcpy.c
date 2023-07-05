d7c7c03b158dd697d43c625a22517ecc53c7e39a
ishimww <uwimpayeclementine12@gmail.com>
2023-07-05 15:11:29 -0700

    asp

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,12 @@
 #include "main.h"
-/*
- * _memcpy - a function that copies memory area
- *@i
- *@n: value of bytes to start when adding
- *@src: source
- *@dest: destination
- * Return: Always 0.
+/**
+ * _memcpy - Copies memory area
+ *
+ * @dest: where to copy the memory to
+ * @n: the number of bytes
+ * @src: the memory area to copy from
+ *
+ * Return: a pointer to @dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
